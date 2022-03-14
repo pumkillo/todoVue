7aa426ceff4d865c31822a2aa135e94d3b50149e
Masha Sedova <moshonkasedova@mail.ru>
2022-03-14 22:47:19 +0700

    search and local storage added

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,18 +23,44 @@ let app = new Vue({
             name: "Shopping list",
             todolist: ["milk", "bread"],
             datetime: "12:15",
+            
         }],
+        queryRes: [],
         query: "",
     },
+    mounted() {
+        if (localStorage.getItem('notes')) {
+            try {
+                this.notes = JSON.parse(localStorage.getItem('notes'));
+            } catch (e) {
+                localStorage.removeItem('notes');
+            }
+        }
+        this.queryRes = this.notes;
+    },
+    computed: {
+        queryLength: function(){
+            return this.queryRes.length;
+        },
+    },
+    beforeUpdate() {
+        this.putInLocalStorageNotes();
+    },
     methods: {
+        putInLocalStorageNotes() {
+            const parsed = JSON.stringify(this.notes);
+            localStorage.setItem('notes', parsed);
+        },
         addNewNote() {
             datetime = new Date();
-            this.notes.push({
+            let minutes = datetime.getMinutes() < 10 ? `0${datetime.getMinutes()}` : datetime.getMinutes();
+            let month = datetime.getMonth() + 1 < 10 ? `0${datetime.getMonth() + 1}` : datetime.getMonth() + 1;
+            this.notes.unshift({
                 name: "",
                 color: "#000000",
                 todolist: [],
-                datetime: `${datetime.getHours()}:${datetime.getMinutes()}`,
-            })
+                datetime: `${datetime.getDate()}.${month}.${datetime.getFullYear()}    ${datetime.getHours()}:${minutes}`,
+            });
         },
         deleteNote(index) {
             this.notes.splice(index, 1);
@@ -44,7 +70,17 @@ let app = new Vue({
                 this.notes[index].todolist.push(event.target.value);
                 event.target.value = "";
             }
-
-        }
+        },
+        search() {
+            let query = this.query;
+            if(query === "") {
+                this.queryRes = this.notes;
+                return;
+            }
+            queryRes = this.notes.filter(function (obj) {
+                return RegExp(query).test(obj.name);
+            })
+            this.queryRes = queryRes;
+        },
     }
 })
